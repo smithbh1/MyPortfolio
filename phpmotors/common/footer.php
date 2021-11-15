28e3e1b31244ac6943deff7601140435d19292b5
smithbh1 <71277422+smithbh1@users.noreply.github.com>
2021-11-15 15:15:11 -0700

    Delete phpmotors directory

--- phpmotors/common/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-     <p><span>&copy;</span>PHP Motors, All rights reserved. All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed. </p>
-    
-    
-</footer>
